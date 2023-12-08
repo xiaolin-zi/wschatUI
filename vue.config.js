2404e9ecb7a916f25310191d6913d8883c23f7e9
xiaolin <1849569695@qq.com>
2023-12-08 22:48:35 +0800

    111

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
         target: 'https://chat-back.tobeyou.cn', // 注意！此处为后端提供的真实接口
         changeOrigin: true, // 允许跨域
         pathRewrite: {
-          '^/https://chat-back.tobeyou.cn/api': ''
+          '^/https://chat-back.tobeyou.cn/api': '' // 重写路径
         }
       }
     }
