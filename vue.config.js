1819a7f0d3f13657aa22f4895a17c38ef9880cfa
xiaolin <1849569695@qq.com>
2023-12-08 22:56:00 +0800

    111

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,15 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  devServer: {
-    proxy: {
-      'https://chat-back.tobeyou.cn/api': {
-        target: 'https://chat-back.tobeyou.cn', // 注意！此处为后端提供的真实接口
-        changeOrigin: true, // 允许跨域
-        pathRewrite: {
-          '^/https://chat-back.tobeyou.cn/api': '' // 重写路径
-        }
-      }
-    }
-  }
+  // devServer: {
+  //   proxy: {
+  //     'https://chat-back.tobeyou.cn/api': {
+  //       target: 'https://chat-back.tobeyou.cn', // 注意！此处为后端提供的真实接口
+  //       changeOrigin: true, // 允许跨域
+  //       pathRewrite: {
+  //         '^/https://chat-back.tobeyou.cn/api': '' // 重写路径
+  //       }
+  //     }
+  //   }
+  // }
 })
