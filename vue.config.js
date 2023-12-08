8140adcb04702a93ad41e6791db585d8c19d736f
xiaolin <1849569695@qq.com>
2023-12-08 21:45:58 +0800

    111

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,17 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  server: {
+  devServer: {
     proxy: {
-      "/api": {
-        target: "chat-back.tobeyou.cn",
-        changeOrigin: true,
-        rewrite: (path) => path.replace("/api", ""),
-      },
+      '/api': {
+        target: 'https://chat-back.tobeyou.cn', // 注意！此处为后端提供的真实接口
+        changeOrigin: true, // 允许跨域
+        pathRewrite: {
+          // 如果接口中是没有api的，那就直接置空，'^/api': ''，
+          // 如果接口中有api，那就得写成{'^/api':'/api'}
+          '^/api': ''
+        }
+      }
     }
-  },
+  }
 })
