e5bccae650670b575790a8b82461f90f613703d3
xiaolin <1849569695@qq.com>
2023-12-08 21:58:28 +0800

    111

--- vue.config.js
+++ vue.config.js
@@ -4,12 +4,10 @@ module.exports = defineConfig({
   lintOnSave: false,
   devServer: {
     proxy: {
-      '/api': {
+      '/api/**': {
         target: 'https://chat-back.tobeyou.cn', // 注意！此处为后端提供的真实接口
         changeOrigin: true, // 允许跨域
         pathRewrite: {
-          // 如果接口中是没有api的，那就直接置空，'^/api': ''，
-          // 如果接口中有api，那就得写成{'^/api':'/api'}
           '^/api': ''
         }
       }
