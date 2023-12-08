79c2be0d0d0a97525789d8e243954420237d9259
xiaolin <1849569695@qq.com>
2023-12-08 21:14:00 +0800

    111

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false,
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://chat-back.tobeyou.cn/api/',// 后端接口
-        changeOrigin: true, // 是否跨域
-        pathRewrite: {
-          '/api': ''
-        }
-      }
-    }
-  }
+  lintOnSave: false
 })
